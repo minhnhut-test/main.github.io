63a88cfa76cee1e917e64c0f0c9747a1154108d7
minhnhut-test <tran.minh.nhut.29.12.2003@gmail.com>
2023-08-22 13:05:24 +0700

    Test function scroll

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+const navagation = document.getElementById("navagation");
+const notification = document.getElementById("notification");
+const home = document.getElementById("home");
+const heightNotification = notification.offsetHeight;
+const heightHome = home.offsetHeight;
+function position(variable){
+    var postion = variable.getBoundingClientRect();
+    var notificationY = postion.top;
+    return notificationY;
+}
 
+window.addEventListener("scroll", ()=>{
+    if(position(home) < heightNotification){
+        notification.classList.add("fixed");
+        if(position(navagation) <= 0){
+            navagation.classList.add("fixed");
+            home.classList.add("fixed");
+        }
+    }else{
+        notification.classList.remove("fixed");
+        home.classList.remove("fixed");
+        navagation.classList.remove("fixed");
+    }
+    console.log(position(home));
+})
