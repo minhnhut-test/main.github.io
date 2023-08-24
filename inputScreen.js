f1d947893b5fcdb85692b15b6a0589e04cb942c1
minhnhut-test <tran.minh.nhut.29.12.2003@gmail.com>
2023-08-25 02:06:21 +0700

    Done user interface

--- inputScreen.js
+++ inputScreen.js
@@ -1,12 +1,20 @@
+const body = document.querySelector('body');
+
 function sizeOfwindow (){
     widthOfwindows = window.innerWidth;
     if(widthOfwindows  < 640){
-        includeHTML();
+        // includeHTML();
+        body.classList.remove("active");
         console.log("Mobile phone");
     }else if( widthOfwindows >= 640 && widthOfwindows<= 1024){
         console.log("Tablet");
+        body.classList.add("active");
+        document.body.innerHTML ="<h1> Sory bạn hiện tại web đang được phát triển ở giao diện tablet và desktop . Nếu bạn dùng resize về mobile vui lòng load lại trang</h1>";
+       
     }else{
         console.log("Desktop");
+        body.classList.add("active");
+        document.body.innerHTML = "<h1> Sory bạn hiện tại web đang được phát triển ở giao diện tablet và desktop . Nếu bạn dùng resize về mobile vui lòng load lại trang</h1>";
     }
 } 
 window.addEventListener("resize", sizeOfwindow) || window.addEventListener("load",sizeOfwindow);
@@ -15,33 +23,33 @@ window.addEventListener("resize", sizeOfwindow) || window.addEventListener("load
 
 
 
-function includeHTML(){
-    var i, elem, file,xhttp,includeFiles;
-     includeFiles = document.getElementsByTagName("*");
+// function includeHTML(){
+//     var i, elem, file,xhttp,includeFiles;
+//      includeFiles = document.getElementsByTagName("*");
 
-    for(i = 0; i < includeFiles.length;i++){
-        console.log("hello")
-        elem= includeFiles[i];
-        file = elem.getAttribute("data-file");
+//     for(i = 0; i < includeFiles.length;i++){
+//         console.log("hello")
+//         elem= includeFiles[i];
+//         file = elem.getAttribute("data-file");
        
-        if(file){
-            xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function(){
-                if(this.readyState == 4){
-                    if(this.status == 200){
-                        elem.innerHTML = this.responseText;
-                    }
-                    if(this.status == 404){
-                        elem.innerHTML ="Component not found";
-                    }
-                    elem.removeAttribute("data-file");
-                    includeHTML();
-                }
-            }
-            xhttp.open("GET",file,true);
-            xhttp.send();
-            return;
-        }
-    }
-};
+//         if(file){
+//             xhttp = new XMLHttpRequest();
+//             xhttp.onreadystatechange = function(){
+//                 if(this.readyState == 4){
+//                     if(this.status == 200){
+//                         elem.innerHTML = this.responseText;
+//                     }
+//                     if(this.status == 404){
+//                         elem.innerHTML ="Component not found";
+//                     }
+//                     elem.removeAttribute("data-file");
+//                     includeHTML();
+//                 }
+//             }
+//             xhttp.open("GET",file,true);
+//             xhttp.send();
+//             return;
+//         }
+//     }
+// };
  
